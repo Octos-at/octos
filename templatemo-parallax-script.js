0408ef70b0580a7fb6947bef045afec4f15f9823
Octos-at <info@octos.at>
2026-03-13 11:39:52 +0100

    Update templatemo-parallax-script.js

--- templatemo-parallax-script.js
+++ templatemo-parallax-script.js
@@ -5,3 +5,23 @@ if (contactForm) {
         alert('Thank you! Your message has been sent.');
     });
 }
+<script>
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    emailjs.send("service_3kflnuh", "template_un57291)", {
+        name: this.name.value,
+        email: this.email.value,
+        message: this.message.value,
+        to_email: "info@octos.at"
+    })
+    .then(() => {
+        alert("Thank you! Your message has been sent.");
+        this.reset();
+    }, (error) => {
+        alert("Error sending message. Please try again.");
+        console.log(error);
+    });
+});
+</script>
+``
