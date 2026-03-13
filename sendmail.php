c98528e4c2280aabf4d297f279a778486d7c32c5
Octos-at <info@octos.at>
2026-03-13 10:39:40 +0100

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,24 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "info@octos.at";
+    $subject = "New message from website contact form";
+    $body = "Name: $name
+Email: $email
+Message:
+$message";
+    $headers = "From: noreply@octos.at
+";
+    $headers .= "Reply-To: $email
+";
+
+    if(mail($to, $subject, $body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
