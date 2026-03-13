0e2d07b2302b4de7ae61c59317a676301dbda51d
Octos-at <info@octos.at>
2026-03-13 10:12:11 +0100

    Add files via upload

---
+++ templatemo-parallax-script.js
@@ -0,0 +1,7 @@
+// Contact form submission
+var contactForm = document.getElementById('contactForm');
+if (contactForm) {
+    contactForm.addEventListener('submit', function () {
+        alert('Thank you! Your message has been sent.');
+    });
+}
